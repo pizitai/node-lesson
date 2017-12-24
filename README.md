57777dd29ea2f9ea5cde33a75c3cf7bb6e443265
wuzhangpeng <wzhangpeng1989@163.com>
2017-12-24 10:18:42 +0800

    add

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # node-lesson
 node学习记录  
+- 需要先启动本地的redis客户端
+- 只检查连接在线，不检查什么时候离线
 
 branch|desc|time
 ---|---|---
-hello_world|练手demo|2017-12-22
+online|在线状态|2017-12-24
--- app.js
+++ app.js
@@ -1,9 +1,46 @@
 var express = require('express');
 
 var app = express();
+var online = require('online');
+var redis = require('redis');
+var db = redis.createClient();
 
-app.get('/', function(req, res){
-  res.send('Hello World');
+// online
+
+online = online(db);
+
+// app
+
+var app = express();
+
+// activity tracking, in this case using
+// the UA string, you would use req.user.id etc
+
+app.use(function(req, res, next){
+  // fire-and-forget
+  online.add(req.headers['user-agent']);
+  next();
+});
+
+/**
+ * List helper.
+ */
+
+function list(ids) {
+  return '<ul>' + ids.map(function(id){
+    return '<li>' + id + '</li>';
+  }).join('') + '</ul>';
+}
+
+/**
+ * GET users online.
+ */
+
+app.get('/', function(req, res, next){
+  online.last(5, function(err, ids){
+    if (err) return next(err);
+    res.send('<p>Users online: ' + ids.length + '</p>' + list(ids));
+  });
 });
 
 /* istanbul ignore next */
