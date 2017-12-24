49bab9a21a033555cb1855040ea0fbc18f2eb3fd
wuzhangpeng <wzhangpeng1989@163.com>
2017-12-24 16:31:45 +0800

    add

--- app.js
+++ app.js
@@ -1,64 +1,12 @@
 var express = require('express');
 
-var verbose = process.env.NODE_ENV != 'test';
-
 var app = module.exports = express();
 
-app.map = function (a, route) {
-  route = route || '';
-  for (var key in a) {
-    switch (typeof a[key]) {
-      // { '/path': { ... }}
-      case 'object':
-        app.map(a[key], route + key);
-        break;
-        // get: function(){ ... }
-      case 'function':
-        if (verbose) console.log('%s %s', key, route);
-        app[key](route, a[key]);
-        break;
-    }
-  }
-};
+app.use('/api/v1', require('./controllers/api_v1'));
+app.use('/api/v2', require('./controllers/api_v2'));
 
-var users = {
-  list: function (req, res) {
-    res.send('user list');
-  },
-
-  get: function (req, res) {
-    res.send('user ' + req.params.uid);
-  },
-
-  delete: function (req, res) {
-    res.send('delete users');
-  }
-};
-
-var pets = {
-  list: function (req, res) {
-    res.send('user ' + req.params.uid + '\'s pets');
-  },
-
-  delete: function (req, res) {
-    res.send('delete ' + req.params.uid + '\'s pet ' + req.params.pid);
-  }
-};
-
-app.map({
-  '/users': {
-    get: users.list,
-    delete: users.delete,
-    '/:uid': {
-      get: users.get,
-      '/pets': {
-        get: pets.list,
-        '/:pid': {
-          delete: pets.delete
-        }
-      }
-    }
-  }
+app.get('/', function(req, res) {
+  res.send('Hello form root route.');
 });
 
 /* istanbul ignore next */
