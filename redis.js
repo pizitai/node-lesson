99d0f6a21a4c993159d8ed66a1b5797ddc2615f6
wuzhangpeng <wzhangpeng1989@163.com>
2017-12-24 15:44:27 +0800

    add

--- redis.js
+++ redis.js
@@ -1,14 +1,25 @@
-var express = require('express');
+/**
+ * Module dependencies.
+ */
 
+var express = require('../..');
+var logger = require('morgan');
 var session = require('express-session');
 
+// pass the express to the connect redis module
+// allowing it to inherit from session.Store
+var RedisStore = require('connect-redis')(session);
+
 var app = express();
+
+app.use(logger('dev'));
 
 // Populates req.session
 app.use(session({
   resave: false, // don't save session if unmodified
   saveUninitialized: false, // don't create session until something stored
-  secret: 'keyboard cat'
+  secret: 'keyboard cat',
+  store: new RedisStore
 }));
 
 app.get('/', function(req, res){
@@ -22,8 +33,5 @@ app.get('/', function(req, res){
   res.send(body + '<p>viewed <strong>' + req.session.views + '</strong> times.</p>');
 });
 
-/* istanbul ignore next */
-if (!module.parent) {
-  app.listen(3000);
-  console.log('Express started on port 3000');
-}
+app.listen(3000);
+console.log('Express app started on port 3000');
